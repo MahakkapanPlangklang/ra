81bcb0d236fb10abedca273a27118c3fd986272a
MahakkapanPlangklang <119285823+MahakkapanPlangklang@users.noreply.github.com>
2023-12-18 07:53:32 +0700

    Update

--- 5.3.cpp
+++ 5.3.cpp
@@ -2,48 +2,23 @@
 
 int main() {
     
-    double num1, num2, result;
-    char mark;
+    int n;
+
+    std::cout << "Enter the value of n (1-20): ";
+    std::cin >> n;
 
     
-    std::cout << "Enter number 1: ";
-    std::cin >> num1;
-
-    std::cout << "Enter number 2: ";
-    std::cin >> num2;
-
-    std::cout << "Enter operator: ";
-    std::cin >> mark;
-
-    
-    switch (mark) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                std::cout << "Cannot divide by zero!" << std::endl;
-                return 1;  
-            }
-            break;
-        case '>':
-            std::cout << "" << std::endl;
-            return 0;  
-        default:
-            std::cout << "Invalid mark!!" << std::endl;
-            return 1;  
+    if (n < 1 || n > 20) {
+        std::cout << "Invalid input. Please enter a value between 1 and 20." << std::endl;
+        return 1;  
     }
 
     
-    std::cout << "Result: " << result << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cout << i % 2 << " ";
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
